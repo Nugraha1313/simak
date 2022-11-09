6f76de488a0c46e84ab6aeffdfda6ef336798be1
Nugraha1313 <aulia.nugraha13@gmail.com>
2022-11-09 18:38:42 +0700

    add admin files

---
+++ resources/views/admin/Dosen.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.index')
+@section('content')
+<div class="container-fluid py-4">
+    <div class="card">
+        <div class="card-header">
+            <h1>Data Dosen</h1>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/JadwalKuliah.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.index')
+@section('content')
+<div class="container-fluid py-4">
+    <div class="card">
+        <div class="card-header">
+            <h1>Jadwal Kuliah</h1>
+        </div>
+    </div>
+</div>
+@endsection

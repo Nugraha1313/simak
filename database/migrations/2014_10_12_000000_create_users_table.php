0e8e56ee1675f16e534b19520ece156184e2966f
FathimahN <fath20233ti@student.nurulfikri.ac.id>
2022-11-22 20:22:14 +0700

    menambahkan username  & menghapus email pada migration dan  seeder users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->unique();
+            // $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
